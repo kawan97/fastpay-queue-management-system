65ad886e03fd098168854ee736d1563591673845
kawan97 <pshtiwankawan@gmail.com>
2022-04-12 07:14:50 -0700

    create a page  to update status code

---
+++ api_update_status.php
@@ -0,0 +1,46 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+
+//array to hold the returned values from the DB
+require_once "dbcon.php";
+
+//query to return the values
+try{
+    $id=addslashes((htmlentities($_GET['id'])));
+    $sql="select * from service WHERE id = ?;";   
+    $execu=$pdo->prepare($sql);
+    $execu->execute((array($id)));
+    $row = $execu->fetch();
+
+    if(!$row){
+        echo json_encode(array("message" => 'Sorry you have dont have that service'));
+        http_response_code(403);  
+    }else{
+        $sql="UPDATE service
+        SET status = ?
+      WHERE id = ?;";   
+       $execu=$pdo->prepare($sql);
+       $execu->execute((array('finished',$id)));
+       if($execu){
+       echo json_encode(array("message" => 'successfully status code changed'));
+       http_response_code(201);  
+       }else{
+           echo json_encode(array("message" => 'Sorry you have an error'));
+           http_response_code(500);  
+   
+       }
+    }
+
+ 
+
+}catch(Exception $e){
+    echo json_encode(array("message" => $e));
+    http_response_code(500);  
+}
+
+
+
+//return the json encoded values
+
